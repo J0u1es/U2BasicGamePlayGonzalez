bbf833fa6b0feaa7fa21afea0f31d0ca230bd554
Julia Gonzalez <145380988+J0u1es@users.noreply.github.com>
2024-03-20 11:38:24 -0700

    Finished adding rigidbodies to the animals
    I added the animals a rigidbody and made them all have a trigger. Just need to do it with the projectile.

--- U2BasicGamePlayGonzalezP3/Assets/Scripts/SpawnManager.cs
+++ U2BasicGamePlayGonzalezP3/Assets/Scripts/SpawnManager.cs
@@ -7,20 +7,26 @@ public class SpawnManager : MonoBehaviour
     private float spawnRangeX = 20;
     private float spawnPosZ = 20;
     public GameObject[] animalPrefabs;
+    private float spawnInterval = 1.5f;
+    private float startDelay = 2;
+
         // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.S)) 
-        {
-            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
-            int animalIndex = Random.Range(0, animalPrefabs.Length);
-            Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
-        }
+       
     }
+    void SpawnRandomAnimal() 
+    {
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+        Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation); ;
+    }
+
 }
+
